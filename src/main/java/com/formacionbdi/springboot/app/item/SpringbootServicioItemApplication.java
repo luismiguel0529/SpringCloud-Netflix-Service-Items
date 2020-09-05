53683e225dbf2e085fa134e647c2bedf8b2bb2d7
Luis Miguel Rodriguez Herrera <l.miguel0529@hotmail.com>
2020-09-05 17:40:45 -0500

    Add exlusion DataSource

--- src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplication.java
+++ src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplication.java
@@ -1,7 +1,9 @@
 package com.formacionbdi.springboot.app.item;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
@@ -12,6 +14,9 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableEurekaClient // balanceo de carga
 @EnableFeignClients // cliente http
 @SpringBootApplication
+
+// la libreria tiene Jpa por ende este debe ser exclido porque este proyecto no maneja persistencia.
+@EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
 public class SpringbootServicioItemApplication {
 
 	public static void main(String[] args) {
